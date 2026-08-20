d06ecc257d82a450562739963e0af5086e7a5bcf
muhammed-anees-pp <muhammedanees.work@gmail.com>
2026-08-20 12:30:37 +0530

    feat: add voice agent communication flow structure

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+import base64
 import json
 import websockets
 import os
@@ -27,3 +29,31 @@ def load_agent_config():
     with open("config.json", "r") as f:
         return json.load(f)
 
+async def handle_user_interrupt(decoded, twilio_ws, streamsid):
+    pass
+
+
+async def handle_deepgram_message(decoded, twilio_ws, deepgram_ws, streamsid):
+    pass
+
+async def send_audio_to_deepgram(deepgram_ws, audio_queue):
+    pass
+
+async def receive_from_deepgram(deepgram_ws, twilio_ws, streamsid_queue):
+    pass
+
+async def receive_from_twilio(twilio_ws, audio_queue, streamsid_queue):
+    pass
+
+
+async def handle_twilio_connection(twilio_ws):
+    pass
+
+async def main():
+    await websockets.serve(handle_twilio_connection, "localhost", 5000)
+    print("Started server")
+    await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
